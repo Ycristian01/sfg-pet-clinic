9cd7dd6de1b4b13e694a39635bd02b149cb3cf44
Ycristian01 <ycristian@uninorte.edu.co>
2023-02-07 16:20:37 -0500

    dependency injection task completed

--- sfg-pet-clinic/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ sfg-pet-clinic/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -11,35 +11,30 @@ public class SfgPetClinicApplication {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(SfgPetClinicApplication.class, args);
 
-        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
-
         System.out.println("----------------- Profile bean");
+        I18nController i18nController = (I18nController) ctx.getBean("i18nController");
         System.out.println(i18nController.sayHello());
 
-        MyController myController = (MyController) ctx.getBean("myController");
-
         System.out.println("----------------- PRIMARY bean");
+        MyController myController = (MyController) ctx.getBean("myController");
         String greeting = myController.sayHello();
-
         System.out.println(greeting);
 
         System.out.println("----------------- Property -----------------");
-
         PropertyInjectedController propertyInjectedController = (PropertyInjectedController) ctx.getBean("propertyInjectedController");
-
         System.out.println(propertyInjectedController.getGreeting());
 
         System.out.println("----------------- Setter -----------------");
-
         SetterInjectedController setterInjectedController = (SetterInjectedController) ctx.getBean("setterInjectedController");
-
         System.out.println(setterInjectedController.getGreeting());
 
         System.out.println("----------------- Constructor -----------------");
-
         ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx.getBean("constructorInjectedController");
-
         System.out.println(constructorInjectedController.getGreeting());
+
+        System.out.println("------------- TASK -------------");
+        PetController petController = (PetController) ctx.getBean("petController");
+        System.out.println(petController.whichPetIsTheBest());
     }
 
 }
